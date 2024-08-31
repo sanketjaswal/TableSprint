7b12c96d28c47e18737e2488f4c9b5d4c8f53ea4
sanketjaswal <sanket.jaswal123@.com>
2024-08-31 18:24:43 +0530

    Added Header

---
+++ client/src/components/header.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import "./header.css";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <h1>TableSprint</h1>
+      <img src="../assets/profile.png" alt="Img NA"></img>
+    </div>
+  );
+};
